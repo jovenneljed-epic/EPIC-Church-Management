ae1c9114964c59aeba4045f68b0cc93327dc55fb
unknown <Client@Client-01.TESDA.NET>
2026-08-10 17:16:35 +0800

    Restore MemberId relationship on User

--- EPIC.Api/Models/User.cs
+++ EPIC.Api/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EPIC.Api.Models
 {
@@ -18,11 +19,14 @@ namespace EPIC.Api.Models
         [MaxLength(150)]
         public string FullName { get; set; } = "";
 
-        // Foreign key to Roles table
         public int RoleId { get; set; }
 
-        // Navigation property
         public Role? Role { get; set; }
+
+        // Links MEMBER user account to Members.MemberId
+        public int? MemberId { get; set; }
+
+        public Member? Member { get; set; }
 
         public bool IsActive { get; set; } = true;
 
